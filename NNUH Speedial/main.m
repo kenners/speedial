0a0c96bf653e77cab42878368ba1525ab71cc388
Kenrick Turner <kenrickturner@gmail.com>
2012-03-14 13:51:49 +0000

    Updating copyright headers. Closes #8 for now.
    May decided to open source at later date.

--- NNUH Speedial/main.m
+++ NNUH Speedial/main.m
@@ -3,7 +3,7 @@
 //  NNUH Speedial
 //
 //  Created by Kenrick Turner on 06/03/2012.
-//  Copyright (c) 2012 Norfolk & Norwich University Hospital. All rights reserved.
+//  Copyright (c) 2012 Kenrick Turner. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
